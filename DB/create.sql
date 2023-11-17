9a3c6c72636a70ff5d775bac22a3e78b14678782
AIsamet <isamet2067@gmail.com>
2023-11-17 23:19:05 +0100

    created the api #13

--- DB/create.sql
+++ DB/create.sql
@@ -1,5 +1,5 @@
 /*  
-Une fois que vous avez installé MySQL :
+Une fois que vous avez installé MySQL, pour tester en local :
 sudo mysql -u root
 CREATE USER 'alcatel'@'localhost' IDENTIFIED BY 'alcatel';
 CREATE DATABASE PTT; 
@@ -13,15 +13,16 @@ create table bubble_location
 (
     bubble_GUID int             not null    primary key,
     name        varchar(255)    null,
-    lattitude   decimal(9,6)    not null,
+    latitude    decimal(9,6)     not null,
     longitude   decimal(9,6)    not null,
     creator     varchar(255)    not null
 );
 
-INSERT INTO PTT.bubble_location (bubble_GUID, name, lattitude, longitude, creator)
+INSERT INTO PTT.bubble_location (bubble_GUID, name, latitude, longitude, creator)
 VALUES
     (1, 'Cathédrale Notre-Dame', 48.581356, 7.750776, 'Pierre Dubois'),
     (2, 'La Petite France', 48.579457, 7.751555, 'Sophie Martin'),
     (3, 'Palais Rohan', 48.581199, 7.749616, 'François Leclerc'),
     (4, 'Ponts couverts', 48.579169, 7.748985, 'Isabelle Dupont'),
     (5, 'Place Kléber', 48.583207, 7.748310, 'Antoine Leroux');
+
